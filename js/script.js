1ce63e95d60f57a6b959a3bc1356420c30168562
Irina Pasinkova <suntunartist@mail.ru>
2023-04-04 14:02:23 +0300

    готово

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger')
+const menu =document.querySelector('.menu')
+function toggleMenu(){
+burger.classList.toggle('open');
+menu.classList.toggle('open');
+}
